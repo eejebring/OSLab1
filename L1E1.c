12a57377ff2c0843b8212d2e8a23b36b2afeee02
eejebring <dev@ejebring.com>
2023-11-15 13:40:26 +0100

    led lights

---
+++ L1E1.c
@@ -0,0 +1,43 @@
+
+
+int LED_R = 28;
+int LED_G = 29;
+int LED_B = 27;
+
+int pin_invert(int pin_state) {
+    if (pin_state == LOW) return HIGH;
+    else return LOW;
+}
+
+void * ex1_red(void * arg) {
+    useconds_t green_sleep_us = 1000 * 1000; // 0.5s
+    int v = LOW;
+    while(running) {
+        v = pin_invert(v);
+        digitalWrite(LED_R, v);
+        usleep(green_sleep_us);
+    }
+    return NULL;
+}
+
+void * ex1_green(void * arg) {
+    useconds_t green_sleep_us = 500 * 1000; // 0.5s
+    int v = LOW;
+    while(running) {
+        v = pin_invert(v);
+        digitalWrite(LED_G, v);
+        usleep(green_sleep_us);
+    }
+    return NULL;
+}
+
+void * ex1_blue(void * arg) {
+    useconds_t green_sleep_us = 200 * 1000; // 0.5s
+    int v = LOW;
+    while(running) {
+        v = pin_invert(v);
+        digitalWrite(LED_B, v);
+        usleep(green_sleep_us);
+    }
+    return NULL;
+}
---
+++ L1E1.h
@@ -0,0 +1,5 @@
+
+int pin_invert(int pin_state);
+void * ex1_red(void * arg);
+void * ex1_green(void * arg);
+void * ex1_blue(void * arg);
