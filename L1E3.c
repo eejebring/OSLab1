2a395f04bd9f5618f34349f54a881a52f651b649
eejebring <dev@ejebring.com>
2023-11-17 10:22:05 +0100

    exercise 3

---
+++ L1E3.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <wiringPi.h>
+#include <lttng/tracef.h>
+
+int LED_R = 28;
+int LED_G = 29;
+int LED_B = 27;
+int running = 1;
+
+uint32_t r_period_ms = 1000;
+useconds_t r_period_us = r_period_ms * 1000;
+uint32_t r_stress_ms = (uint32_t) (0.2 * (float) r_period_ms);
+
+uint32_t g_period_ms = 500;
+useconds_t g_period_us = g_period_ms * 1000;
+uint32_t g_stress_ms = (uint32_t) (0.2 * (float) g_period_ms);
+
+uint32_t b_period_ms = 200;
+useconds_t b_period_us = b_period_ms * 1000;
+uint32_t b_stress_ms = (uint32_t) (0.2 * (float) b_period_ms);
+
+int pin_invert(int pin_state) {
+    if (pin_state == LOW) return HIGH;
+    else return LOW;
+}
+
+void * ex1_red(void * arg) {
+    int v = LOW;
+    while(running) {
+        v = pin_invert(v);
+        tracef("Stressing for: %u ms", r_stress_ms);
+        cpu_stress(r_stress_ms);
+        tracef("RED LED = %d", v);
+        digitalWrite(LED_R, v);
+        tracef("sleep(RED, %u usec)", r_period_us);
+        usleep(r_period_us);
+    }
+    return NULL;
+}
+
+void * ex1_green(void * arg) {
+    int v = LOW;
+    while(running) {
+        v = pin_invert(v);
+        tracef("Stressing for: %u ms", g_stress_ms);
+        cpu_stress(g_stress_ms);
+        tracef("GREEN LED = %d", v);
+        digitalWrite(LED_G, v);
+        tracef("sleep(GREEN, %u usec)", g_period_us);
+        usleep(g_period_us);
+    }
+    return NULL;
+}
+
+void * ex1_blue(void * arg) {
+    int v = LOW;
+    while(running) {
+        v = pin_invert(v);
+        tracef("Stressing for: %u ms", b_stress_ms);
+        cpu_stress(b_stress_ms);
+        tracef("BLUE LED = %d", v);
+        digitalWrite(LED_B, v);
+        tracef("sleep(BLUE, %u usec)", b_period_us);
+        usleep(b_period_us);
+    }
+    return NULL;
+}
+
+int main()
+{
+    wiringPiSetup();
+
+    pthread_t t1;
+    pthread_t t2;
+    pthread_t t3;
+    pthread_create(&t1, NULL, ex1_red, NULL);
+    pthread_create(&t2, NULL, ex1_green, NULL);
+    pthread_create(&t3, NULL, ex1_blue, NULL);
+
+    usleep(20000000);
+    running = 0;
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+
+    printf("\nAll threads are done!\n");
+    return EXIT_SUCCESS;
+}
