dbf550841cfffccb27c0eb08b7b28cb436bb1c53
eejebring <dev@ejebring.com>
2023-11-17 11:25:52 +0100

    testing times

--- L1E4.c
+++ L1E4.c
@@ -62,7 +62,7 @@ void * ex_green(void * arg) {
         tracef("GREEN LED = %d", v);
         digitalWrite(LED_G, v);
         tracef("sleep(GREEN, %u usec)", g_period_ns);
-        nanosleep(CLOCK_REALTIME, tEnd);
+        nanosleep(&tEnd, &tEnd);
     }
     return NULL;
 }
