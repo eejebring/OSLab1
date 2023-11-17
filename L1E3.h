f7623a9fab848892ee3478486384461586887d25
eejebring <dev@ejebring.com>
2023-11-17 10:30:08 +0100

    h file

--- L1E3.h
+++ L1E3.h
@@ -1,5 +1,5 @@
 
 int pin_invert(int pin_state);
-void * ex1_red(void * arg);
-void * ex1_green(void * arg);
-void * ex1_blue(void * arg);
+void * ex3_red(void * arg);
+void * ex3_green(void * arg);
+void * ex3_blue(void * arg);
