db1a28fa799be4913515cd843bdfd1650755bd3a
eejebring <dev@ejebring.com>
2023-11-17 12:17:29 +0100

    micro

--- L1E4.c
+++ L1E4.c
@@ -69,7 +69,7 @@ int * ex_green(void * arg) {
 
 void * ex_blue(void * arg) {
     uint32_t b_period_ms = 1000;
-    useconds_t b_period_ns = b_period_us * 1000;
+    useconds_t b_period_us = b_period_ms * 1000;
     uint32_t b_stress_ms = (uint32_t) (0.2 * (float) b_period_ms);
 
     struct timespec delay;
